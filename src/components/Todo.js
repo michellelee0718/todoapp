caacc7605cbfaa8a921555bf741dada6a48de091
Michelle Lee <68783320+michellelee0718@users.noreply.github.com>
2022-09-28 21:13:50 -0700

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -16,8 +16,21 @@ const Todo = ({text, todo, todos, setTodos}) => {
         })
         );
     };
+    
+    // change color depending on what the task is
+    const changeColor = (text) => {
+        var newBg = "todo";
+        if (text.indexOf("lecture") !== -1 || text.indexOf("notes") !== -1 || text.indexOf("homework") !== -1 || text.indexOf("study") !== -1) {
+            newBg = "todoSchool";
+        } else if (text.indexOf("gym") !== -1) {
+            newBg = "todoGym";
+        }
+        return newBg;
+    };
+    
+    
     return (
-        <div className="todo">
+        <div className={changeColor(newBg)}> // originally <div className="todo"> before deciding on color changing thing
             <li className = {`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
                 <button onClick={completeHandler} className = "complete-btn">
                    <i className="fas fa-check"></i>
